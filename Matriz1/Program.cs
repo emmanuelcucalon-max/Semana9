512c36df035c665698cb935cec8813f0d4183f3e
emmanuelcucalon-max <ercucalon@uamv.edu.ni>
2026-05-13 12:28:43 -0600

    feat: Ejercicio matriz1

---
+++ Matriz1/Program.cs
@@ -0,0 +1,41 @@
+﻿double[,] matriz = new double [3,3];
+
+Console.WriteLine("Matrices");
+Console.WriteLine("Ingresa las temperaturas en la matriz");
+int count=1;
+for(int r=0; r<3; r++)
+{
+    for(int c=0;c<3;c++)
+    {
+        try
+        {
+            Console.WriteLine($"Dime la temperatura: #{count++}");
+            matriz[r,c]=Convert.ToDouble(Console.ReadLine()!);
+        }
+        catch(FormatException)
+        {
+            Console.ForegroundColor=ConsoleColor.Red;
+            Console.WriteLine("Formato de entrada incorrecto, Ingrese numeros enteros");
+            Console.ResetColor();
+            c--;
+            count--;
+        }
+        
+    }
+}
+Console.Clear();
+Console.WriteLine("Temperaturas registradas");
+for(int f=0; f<3; f++)
+{
+    for(int c=0;c<3;c++)
+    {
+        if(matriz[f,c]>=40) Console.ForegroundColor=ConsoleColor.Red;
+        else if (matriz[f,c]>=35) Console.ForegroundColor=ConsoleColor.Yellow;
+        else Console.ForegroundColor=ConsoleColor.Green;
+
+        Console.Write($"{matriz[f,c]} ");
+        Console.ResetColor();
+        
+    }
+    Console.WriteLine();
+}
